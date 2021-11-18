31e311c1a820eb8c18ab803961f3c6f3fa19770e
LindsaySenia <lmzsenia@gmail.com>
2021-11-17 22:13:02 -0500

    indexjs fix

---
+++ client/main.js
@@ -0,0 +1,1 @@
+console.log('well this isn"t the problem')
--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+app.use(express.static('client'));
+
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
-
 const port = process.env.PORT || process.env.SERVER_PORT;
 
 app.listen(port, () => console.log(`I'm all ${port}s... Groovy baby, port is up!`));
